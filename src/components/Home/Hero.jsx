5f5a9a14fda9859e5bfec7e20ff6180c8799cb06
_ArrietA_ <andresarrieta1111@gmail.com>
2023-08-20 08:43:16 -0600

    1.0.2

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,7 +3,7 @@ import { Form } from "./Form/Form";
 
 export const Hero = () => {
   return (
-    <div className={`flex items-center justify-center h-[35vh] max-h-[500px] min-h-[230px] px-6 rounded-tr-3xl rounded-tl-3xl bg-[url('../../public/cat.avif')] bg-cover bg-no-repeat bg-center md:justify-start md:px-14 md:h-[88vh]`}>
+    <div className={`cat flex items-center justify-center h-[35vh] max-h-[500px] min-h-[230px] px-6 rounded-tr-3xl rounded-tl-3xl md:justify-start md:px-14 md:h-[88vh]`}>
       <div className="flex flex-col gap-4 w-full">
         <Logo color={"white"} className={"text-5xl text-white md:text-6xl"} size="w-48 md:w-64" />
         <span className="font-medium text-lg text-gray-100">Get to know more about your <br className="" /> cat breed</span>
